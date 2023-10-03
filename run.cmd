de46fe3fe171e2c3a040e9be3923fb52b0828a5c
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-03 17:49:57 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,5 +1,5 @@
 @echo off
 set PATH=%USERPROFILE%\Downloads\putty
 plink -i xxx.ppk -no-antispoof -ssh ubuntu@xxxxxxxxxxxx.compute.amazonaws.com 'rm -rf ./public/*'
-pscp -i xxx.ppk "New Text Document.txt" ubuntu@xxxxxxxxxxxx.compute.amazonaws.com:/home/ubuntu/public/
+pscp -i xxx.ppk -r "./dist/" ubuntu@xxxxxxxxxxxx.compute.amazonaws.com:/home/ubuntu/public/
 pause
